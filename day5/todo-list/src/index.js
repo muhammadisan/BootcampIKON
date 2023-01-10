796f31e6c8362fc17b000c285f48d45fd3907ff7
Muhammad Ihsan <muh.ihsan@elnusa.co.id>
2023-01-10 15:21:12 +0700

    day 5

---
+++ day5/todo-list/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+import AppHeader from './AppHeader';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Form from './pages/Form';
+import NotFound from './pages/NotFound';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <div><AppHeader /></div>
+      <Routes>
+        <Route path='*' element={<NotFound />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/notfoundroute" element={<About />}></Route>
+        <Route path="/form" element={<Form />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ day5/todo-list/src/pages/Home.js
@@ -0,0 +1,118 @@
+import './Home.css';
+import React, { useState } from 'react';
+
+export default function Home() {
+  const [state, setState] = useState([
+    {
+      id: 1,
+      task: 'Bermain permainan papan',
+      description: 'Bermain sama Patrick dan Squidward',
+      completed: false,
+      deleted: false
+    },
+    {
+      id: 2,
+      task: 'Bermain tic tac toe',
+      description: 'Bermain melawan Patrick',
+      completed: false,
+      deleted: false
+    },
+    {
+      id: 3,
+      task: 'Minum cokelat panas',
+      description: 'Minum cokelat panas sore hari',
+      completed: false,
+      deleted: false
+    },
+    {
+      id: 4,
+      task: 'Menyelesaikan puzzle',
+      description: 'Menyelesaikan jigsaw',
+      completed: false,
+      deleted: false
+    },
+    {
+      id: 5,
+      task: 'Menonton TV',
+      description: 'Menonton berita Bikini Bottom sama Gary',
+      completed: false,
+      deleted: false
+    },
+  ]);
+
+  const [newTask, setNewTask] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newTask) {
+      let copy = [...state];
+      copy = [...copy, { id: state.length + 1, task: newTask, description: newDescription, completed: false, deleted: false }];
+      setState(copy);
+      setNewTask('');
+      setNewDescription('');
+    }
+
+    return;
+  }
+
+  return (
+    <div className="Home">
+      <div className="Todo">
+        <h1>Todos</h1>
+        <form onSubmit={handleSubmit}>
+          <input className="input-todo" type="text" value={newTask} placeholder="Task" onChange={(e) => setNewTask(e.target.value)} />
+          <input className="input-todo" type="text" value={newDescription} placeholder="Task description" onChange={(e) => setNewDescription(e.target.value)} />
+          <input type="submit" value="Add Task" className="button-7" />
+        </form>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Task</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {state.map((todo) => {
+              return (
+                <tr key={todo.id}>
+                  <td><span className={`badge ${todo.deleted ? 'badge--danger' : (todo.completed ? 'badge--success' : 'badge--info')}`}>
+                    {todo.deleted ? 'deleted' : (todo.completed ? 'completed' : 'on going')}
+                  </span></td>
+                  <td>
+                    <div className={`task ${todo.deleted ? 'deleted' : (todo.completed ? 'completed' : '')}`}>{todo.task}</div>
+                    <div className={`description ${todo.deleted ? 'deleted' : (todo.completed ? 'completed' : '')}`}>{todo.description}</div>
+                  </td>
+                  <td>
+                    <button className="button-7" style={{ backgroundColor: "#2ecc71", marginRight: "10px" }} onClick={() => {
+                      const nextState = state.map(a => {
+                        if (a.id !== todo.id) return a;
+                        return { ...a, completed: !todo.completed }
+                      });
+
+                      setState(nextState);
+                    }}>
+                      {todo.completed ? 'Undone' : 'Done'}
+                    </button>
+                    <button className="button-7" style={{ backgroundColor: "#e74c3c" }} onClick={() => {
+                      const nextState = state.map(a => {
+                        if (a.id !== todo.id) return a;
+                        return { ...a, deleted: !todo.deleted }
+                      });
+
+                      setState(nextState);
+                    }}>
+                      {todo.deleted ? 'Undelete' : 'Delete'}
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
